590d877a66d3a5318128a12d74fafad7934f141a
anky faundez soto pena <anky@ankys-MacBook-Air.local>
2024-06-07 13:07:42 -0400

    aloja

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -6,48 +6,71 @@ import { devtools } from 'frog/dev'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 import { abi } from '../abi'
+import axios from 'axios'
 
 const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  imageOptions: {
+    /* Other default options */
+    fonts: [
+      {
+        name: 'Righteous',
+        source: 'google',
+      },
+    ],
+  },
 })
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
 app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
   return c.res({
-    action: "/second",
-    image: (
-      <div
-        style={{
-          color:"white"
-        }}>
-        This is our first frame
-      </div>
-    ),
+    action: "/submit",
+    image: ('https://github.com/jpfraneto/images/blob/main/ankkky.png?raw=true'),
     intents: [
-      <Button>next frame</Button>,
-      <Button.Link href='https://www.anky.bot'>anky</Button.Link>,
+      <TextInput placeholder="what do you want a joke about?" />,
+      <Button value="dad jokes">dad joke</Button>,
     ],
   })
 })
 
-app.frame('/second', (c) => {
+app.frame('/submit', async (c) => { 
+  console.log("on the submit route", process.env)
+
+  const { buttonValue, frameData } = c
+  let fid, text
+  if(frameData){
+    fid = frameData.fid
+    text = frameData.inputText
+  }
+  console.log("the text is: ", text)
+  const response = await axios.post('https://poiesis.anky.bot/joke', {text} ,{
+    headers: {
+      'Authorization': `Bearer ${process.env.POIESIS_API_KEY}`
+    }
+  });
+  
+  
   return c.res({
-    action: "/third",
     image: (
       <div
         style={{
-          color:"white"
+          color:"white",
+          padding: '20px',
+          fontSize: "40px"
         }}>
-        This is our second frame
+        {response.data.joke}
       </div>
     ),
     intents: [
-      <Button>third frame</Button>
+      <Button value="one">one</Button>,
+      <Button value="one">two</Button>,
+      <Button value="one">three</Button>,
     ],
   })
 })
