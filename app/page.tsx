5cf615c64da46868ec3cf4f22fcb6c3e08e7889d
anky faundez soto pena <anky@ankys-MacBook-Air.local>
2024-06-07 15:44:49 -0400

    aloja

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export async function generateMetadata(): Promise<Metadata> {
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
-  )
   // const frameTags = await getFrameMetadata(
-  //   `https://learning-frog-eight.vercel.app//api`,
+  //   `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
   // )
+  const frameTags = await getFrameMetadata(
+    `https://www.bangercaster.xyz/api`,
+  )
   return {
     other: frameTags,
   }
